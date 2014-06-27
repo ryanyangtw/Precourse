d140c913732640ad8e20aa79793a2b269dbe9a49
Ryan <hiphubryan@gmail.com>
2014-06-27 22:19:15 +0800

    finish Variables

---
+++ variables/assignment6.rb
@@ -0,0 +1,3 @@
+# This represent that a variable or method named showes that has not been defined in the right scope
+
+puts "This represent that a variable or method named showes that has not been defined in the right scope"
---
+++ variables/name.rb
@@ -0,0 +1,18 @@
+#1.
+puts "Hey What's your first name?"
+first_name = gets.chomp
+puts "Hello " + first_name + " !"
+
+
+#3.
+10.times do |n|
+	puts first_name
+end
+
+#4. 
+puts "What's your second name?"
+second_name = gets.chomp
+puts "Your full name is " + first_name + " "+ second_name
+
+
+
